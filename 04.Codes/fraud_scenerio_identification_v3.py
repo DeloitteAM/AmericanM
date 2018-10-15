0c90c28546c3ac049570dd18a8383e5f3719f749
deepak1995 <deepak.miglani1995@gmail.com>
2018-10-15 18:46:38 +0530

    15th Oct Update

--- 04.Codes/fraud_scenerio_identification_v3.py
+++ 04.Codes/fraud_scenerio_identification_v3.py
@@ -26,13 +26,12 @@ from plotly.graph_objs import *
 
 import matplotlib.pyplot as plt
 
+from collections import Counter
+
 
 ###############################################################################
 ############### Thresold ######################################################
 num_std_dev = 2
-
-
-
 ################################# Read INI File ###############################
 
 ini_path = 'C:\\Users\\dmiglani\\Desktop\\ModernAmerican\\config_modern_am_oracle.ini'
@@ -56,7 +55,7 @@ os.chdir(proj_path + '/04.Codes/Functions')
 from preprocess import column_preprocess
 os.chdir(proj_path)
 
-intermediate_dir_path = proj_path + "/05.Intertmediate/"
+intermediate_dir_path = proj_path + "/05.Intertmediate/Pekin/"
 del proj_path
 
 
@@ -77,7 +76,8 @@ exec(compile(open(read_db_path, "rb").read(), read_db_path, 'exec'))
 
 cc_claim = cc_claim[['ID','REPORTEDDATE','LITIGATIONSTATUS', 'LOSSCAUSE', 
                      'LOSSDATE', 'ASSIGNEDUSERID', 'INSUREDDENORMID', 
-                     'CLAIMNUMBER', 'POLICYID', 'LOSSTYPE', 'LOSSLOCATIONID']]
+                     'CLAIMNUMBER', 'POLICYID', 'LOSSTYPE', 'LOSSLOCATIONID',
+                     'CATASTROPHEID']]
 
 cc_address = cc_address[['ID', 'ADDRESSLINE1', 'ADDRESSLINE2', 'STATE', 'CITY', 
                          'POSTALCODE']]
@@ -85,7 +85,7 @@ cc_address = cc_address[['ID', 'ADDRESSLINE1', 'ADDRESSLINE2', 'STATE', 'CITY',
 cc_policy = cc_policy[['ID', 'REPORTINGDATE', 'EFFECTIVEDATE', 'EXPIRATIONDATE', 
                        'POLICYNUMBER']]
 
-cc_check = cc_check[['CLAIMID', 'PAYTO', 'REPORTABLEAMOUNT', 'PAYMENTMETHOD', 
+cc_check = cc_check[['ID', 'CLAIMID', 'PAYTO', 'REPORTABLEAMOUNT', 'PAYMENTMETHOD', 
                      'CLAIMCONTACTID']]
 
 cc_exposure = cc_exposure[['ID','COVERAGEID', 'EXAMINATIONDATE', 
@@ -93,16 +93,33 @@ cc_exposure = cc_exposure[['ID','COVERAGEID', 'EXAMINATIONDATE',
                            'LOSTPROPERTYTYPE', 'CREATETIME', 'CLAIMID',
                            'CLAIMANTDENORMID']]
 
+
+cc_contact.columns.values
 cc_contact = cc_contact[['TAXID', 'ID', 'LASTNAME', 'FIRSTNAME', 
-                         'EMPLOYEENUMBER', 'NAME','GREENCARDNUMBER']]
+                         'EMPLOYEENUMBER', 'NAME','GREENCARDNUMBER',
+                         'PRIMARYADDRESSID']]
 
 cc_claimcontact = cc_claimcontact[['ID', 'CONTACTID']]
 
 cctl_losscause = cctl_losscause[['ID', 'DESCRIPTION']]
 
+cc_claimIndicator = cc_claimIndicator[['ID', ]]
+
+cc_checkpayee = cc_checkpayee[['ID', 'CHECKID', 'PAYEEDENORMID', 'CLAIMCONTACTID']]
 
 cc_activity = cc_activity[['CLAIMID', 'CREATETIME', 'UPDATEUSERID']]
 
+cc_user = cc_user[['ID','AUTHORITYPROFILEID']]
+cc_authoritylimit = cc_authoritylimit[['PROFILEID', 'LIMITAMOUNT', 'LIMITTYPE']]
+
+cc_authoritylimit = cc_authoritylimit.loc[cc_authoritylimit['LIMITTYPE'] == 2]
+
+
+cc_claimIndicator = cc_claimIndicator[['CLAIMID', 'SUBTYPE', 'ISON']]
+cc_claimIndicator = cc_claimIndicator.loc[cc_claimIndicator['SUBTYPE'] == 4]
+cc_claimIndicator.pop('SUBTYPE')
+
+cc_claimIndicator['CLAIMID'].nunique()
 ################## Preprocessing Columns ######################################
 cc_exposure.rename(columns={'ID':'EXPOSUREID'}, inplace=True)
 cc_exposure.rename(columns={'CLAIMANTDENORMID':'CONTACTID'}, inplace=True)
@@ -114,7 +131,7 @@ cc_address.rename(columns={'ID':'LOSSLOCATIONID'}, inplace=True)
 cc_policy.rename(columns={'ID':'POLICYID'}, inplace=True)
 cctl_losscause.rename(columns={'ID':'LOSSCAUSE'}, inplace=True)
 cctl_losscause.rename(columns={'DESCRIPTION':'LOSSDESCRIPTION'}, inplace=True)
-
+cc_user.rename(columns={'ID':'USERID'}, inplace=True)
 
 cc_address = column_preprocess(cc_address, ['ADDRESSLINE1'])
 cc_check = column_preprocess(cc_check, ['PAYTO'])
@@ -124,23 +141,68 @@ cc_transaction['CREATETIME'] = pd.to_datetime(cc_transaction["CREATETIME"])
 cc_activity['CREATETIME'] = pd.to_datetime(cc_activity["CREATETIME"])
 
 
-###############################################################################
 
 ################## Merge Data Tables ##########################################
+#cc_authoritylimit2['PROFILEID'].nunique()
+#cc_authoritylimit = cc_authoritylimit[['PROFILEID', 'LIMITAMOUNT']].\
+#    drop_duplicates(keep=False)
+#
+#cc_user = cc_user.merge(cc_authoritylimit, 'left',
+#                        left_on = 'AUTHORITYPROFILEID',
+#                        right_on = 'PROFILEID')
+#cc_user.pop('AUTHORITYPROFILEID')
+
 cc_claim = cc_claim.merge(cc_address, 'left')
 cc_claim = cc_claim.merge(cc_policy, 'left')
+cc_claim = cc_claim.merge(cc_claimIndicator, 'left') 
+
+freq = Counter(cc_claim['ISON'])
+
+#cc_claim = cc_claim.merge(cc_user, 'left', 
+#                          left_on = 'ASSIGNEDUSERID', right_on = 'USERID')
+#cc_claim.rename(columns={'LIMITAMOUNT':'Assigned_LIMITAMOUNT',
+#                         'LIMITTYPE' : 'Assigned_LIMITTYPE'}, 
+#                inplace=True)
 del cc_address, cc_policy
 
 cc_check = cc_check.merge(cc_claimcontact, 'left')
 cc_check = cc_check.merge(cc_claim[['CLAIMID', 'ASSIGNEDUSERID','LOSSCAUSE']], 
                           'left')
+
 cc_check = cc_check.merge(cctl_losscause, 'left')
 cc_check = cc_check.merge(cc_contact[['CONTACTID', 'FIRSTNAME', 
                                       'LASTNAME', 'NAME']], 'left')
-###############################################################################
+
+############## Last Assigned Approval #########################################
+cc_activity = cc_activity.sort_values('CREATETIME', ascending = False)
+cc_activity_last = cc_activity.groupby('CLAIMID', as_index = False).first()
+
+cc_check = cc_check.merge(cc_activity_last[['CLAIMID', 'UPDATEUSERID']], 'left')
+cc_check['LastAssignedUser'] = np.where(np.isnan(cc_check['UPDATEUSERID']), 
+        cc_check['ASSIGNEDUSERID'],
+        cc_check['UPDATEUSERID'])
+
+cc_claim = cc_claim.merge(cc_activity_last[['CLAIMID', 'UPDATEUSERID']], 'left')
+cc_claim['LastAssignedUser'] = np.where(np.isnan(cc_claim['UPDATEUSERID']), 
+        cc_claim['ASSIGNEDUSERID'],
+        cc_claim['UPDATEUSERID'])
+#cc_claim = cc_claim.merge(cc_user, 'left', 
+#                          left_on = 'LastAssignedUser', right_on = 'USERID')
+#cc_claim.rename(columns={'LIMITAMOUNT':'Approval_LIMITAMOUNT',
+#                         'LIMITTYPE' : 'Approval_LIMITTYPE'}, 
+#                inplace=True)
 
 
-#################### Number of approavals for each claim ######################
+#################### Useful Tables ############################################
+claim_payment = cc_check.groupby('CLAIMID', as_index= False).\
+    agg({'REPORTABLEAMOUNT' : 'sum'})
+claim_payment.rename(columns={'REPORTABLEAMOUNT':'ClaimPayment'}, 
+                     inplace=True)
+
+
+cc_claim = cc_claim.merge(claim_payment, 'left')
+cc_claim['ClaimPayment'].fillna(0, inplace = True)
+#################### Number of approvals for each claim ######################
 
 approvals_dat = cc_activity.groupby('CLAIMID', as_index = False).\
     agg({'UPDATEUSERID': 'nunique'})
@@ -150,8 +212,11 @@ cc_claim = cc_claim.merge(approvals_dat, 'left')
 cc_claim['NumApprovals'].fillna(0, inplace = True) 
  
 approvals_summary = cc_claim.groupby('NumApprovals', as_index = False).\
-    agg({'CLAIMID' : 'nunique'})
-approvals_summary.rename(columns={'CLAIMID':'NumClaims'}, inplace=True)
+    agg({'CLAIMID' : 'nunique', 'ClaimPayment' : 'sum'})
+    
+approvals_summary.rename(columns={'CLAIMID':'NumClaims', 
+                                  'ClaimPayment' : 'Payment'}, 
+                         inplace=True)
 
 approvals_summary = approvals_summary.sort_values('NumApprovals', ascending = True)
 
@@ -162,10 +227,12 @@ approvals_summary['NumApprovals'] = np.where(approvals_summary['NumApprovals'] =
                  ">=3", approvals_summary['NumApprovals'])
 
 approvals_summary = approvals_summary.groupby('NumApprovals', as_index = False).\
-    agg({'NumClaims' :'sum'})
+    agg({'NumClaims' :'sum', 'Payment' : 'sum'})
 
-approvals_summary.to_csv(intermediate_dir_path + "Visualization/Pekin_approval_freq.csv", 
-                   encoding='utf-8', index=False)
+approvals_summary['Avg_Payment'] = approvals_summary['Payment']/approvals_summary['NumClaims']
+
+#approvals_summary.to_csv(intermediate_dir_path + "Visualization/Pekin_approval_freq.csv", 
+#                   encoding='utf-8', index=False)
 
 ##  Visualization : Distribution of Number of Approvals 
 # Sort
@@ -193,20 +260,31 @@ fig = dict( data=data, layout=layout )
 
 plot(fig)  
 
-###############################################################################
-############## Last Assigned Approval #########################################
-cc_activity = cc_activity.sort_values('CREATETIME', ascending = False)
-cc_activity_last = cc_activity.groupby('CLAIMID', as_index = False).first()
 
-cc_check = cc_check.merge(cc_activity_last[['CLAIMID', 'UPDATEUSERID']], 'left')
-cc_check['LastAssignedUser'] = np.where(np.isnan(cc_check['UPDATEUSERID']), 
-        cc_check['ASSIGNEDUSERID'],
-        cc_check['UPDATEUSERID'])
+## Visulaization Dollar
+labels = approvals_summary['NumApprovals']
+values = approvals_summary['Payment']
+colors = ['#FEBFB3', '#E1396C', '#96D38C', '#D0F9B1']
 
-cc_claim = cc_claim.merge(cc_activity_last[['CLAIMID', 'UPDATEUSERID']], 'left')
-cc_claim['LastAssignedUser'] = np.where(np.isnan(cc_claim['UPDATEUSERID']), 
-        cc_claim['ASSIGNEDUSERID'],
-        cc_claim['UPDATEUSERID'])
+trace = go.Pie(labels=labels, values=values,
+               hoverinfo='value', textinfo='percent', sort = False,
+               textfont=dict(size=20),
+               marker=dict(colors=colors, 
+                           line=dict(color='#000000', width=2)))
+
+data = [trace]
+layout = Layout(
+    title = "Distribution of Number of Approvals",
+    showlegend=True,
+    height=600,
+    width=600
+)
+
+fig = dict( data=data, layout=layout )
+
+plot(fig)  
+
+
 
 ###############################################################################
 ######################## Fraud Scenerio 3 : Manual Cheque Fraud ###############
@@ -243,8 +321,8 @@ approval_manual_pymt = approval_manual_pymt.merge(approval_pymt, 'left')
 approval_manual_pymt['PercentageManual'] = \
     100 * approval_manual_pymt['Manual_Check_Amt'] / approval_manual_pymt['Ttl_REPORTABLEAMOUNT']
     
-approval_manual_pymt.to_csv(intermediate_dir_path + "Visualization/Test_manual_check.csv", 
-                   encoding='utf-8', index=False)
+#approval_manual_pymt.to_csv(intermediate_dir_path + "Visualization/Test_manual_check.csv", 
+#                   encoding='utf-8', index=False)
 
 Thresold_Perc_Manual_Payment = np.mean(approval_manual_pymt['PercentageManual']) + \
     np.std(approval_manual_pymt['PercentageManual'])
@@ -333,11 +411,51 @@ plot(fig)
 ################################################################################
 ##################### Fraud 4a : Adjustor- Reciever Pair Insured Pair #########
 
+adjustor_payto = cc_check.groupby(['LastAssignedUser'], 
+                                  as_index = False).\
+                                  agg({'PAYTO' : 'unique'})
+                                  
+adjustor_payto.rename(columns={'PAYTO':'all_PAYTO'}, inplace=True)
+adjustor_payto['all_PAYTO'] = adjustor_payto['all_PAYTO'].astype(tuple)
 
-pair_adjustor_receiver = cc_check.groupby(['PAYTO','LastAssignedUser'], 
+pair_adjustor_receiver = cc_check.groupby(['LastAssignedUser', 'PAYTO'], 
                                           as_index = False).\
                                           agg({'REPORTABLEAMOUNT' : 'sum', 
                                                'CLAIMID' : 'nunique'})
+
+pair_adjustor_receiver = pair_adjustor_receiver.merge(adjustor_payto)
+
+pair_adjustor_receiver['FuzzyPayTo'] = ''
+pair_adjustor_receiver['FuzzyPayToScore'] = np.nan
+x = 1
+
+for x in range(len(pair_adjustor_receiver.index)) :
+    temp = process.extract(pair_adjustor_receiver['PAYTO'][x], 
+                           pair_adjustor_receiver['all_PAYTO'][x], 
+                           scorer = fuzz.token_sort_ratio)
+    if (len(temp) > 1):
+        temp = temp[1]
+        pair_adjustor_receiver.loc[x, 'FuzzyPayTo'] = temp[0]
+        pair_adjustor_receiver.loc[x, 'FuzzyPayToScore'] = temp[1]
+
+Thresold_fuzzy = 83
+fuzzy_pair = pair_adjustor_receiver.loc[pair_adjustor_receiver['FuzzyPayToScore'] >= \
+                                        Thresold_fuzzy]
+
+fuzzy_pair = fuzzy_pair[['PAYTO', 'FuzzyPayTo']]
+
+
+#pair_adjustor_receiver = cc_claim_f2.loc[cc_claim_f2['FuzzyAddressScore'] >= Thresold_Address_Fuzzy_Match]
+
+fuzzy_pair['new_col'] = list(zip(fuzzy_pair.PAYTO, fuzzy_pair.FuzzyPayTo))
+
+print (fuzzy_pair.dtypes)
+
+fuzzy_pair['new_col'][37] == fuzzy_pair['new_col'][38]
+
+fuzzy_pair['tuple'] = fuzzy_pair.apply(tuple, axis=1)
+fuzzy_pair.drop_duplicates('tuple')
+
 
 pair_adjustor_receiver = pair_adjustor_receiver.sort_values('REPORTABLEAMOUNT', 
                                                             ascending=False)
@@ -437,3 +555,12 @@ plot(fig)
 
 
 
+############################################# TAX ID #############################
+cc_checkpayee = cc_checkpayee[['ID', 'CHECKID', 'PAYEEDENORMID', 'CLAIMCONTACTID']]
+
+cc_check = cc_check.merge(cc_checkpayee, 'left',
+                          left_on = 'ID', right_on = 'CHECKID')
+
+
+######################################################################################
+###################### Adjustor Address #############################
